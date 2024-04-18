5167b616363cd3c1b41d10848c6fa539f01646ca
Sebah Abubeker <sebahismail21@gmail.com>
2024-04-18 13:14:30 +0200

    nav and header attempt

--- the-weather/src/main.jsx
+++ the-weather/src/main.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+// Removed unused import
+import { Link, Outlet, RouterProvider, createBrowserRouter, } from 'react-router-dom';
+import App from './App';
+import GetLocation from './components/GetLocation.jsx';
+import Weather from './components/WeatherM.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <>
+        <div>
+          <h1>The Weathering Weather!</h1>
+          <Link to="home">Home</Link>|{" "}
+          <Link to="getlocation">Getlocation</Link>|{" "}
+          <Link to="weather">Search weather</Link>|{" "}
+        </div>
+        <Outlet></Outlet>
+      </>
+    ),
+    children: [
+      { path: "/", element: <App></App>},
+      { path: "home", element: <App></App>},
+      { path: "getlocation", 
+      element: <GetLocation></GetLocation>},
+      { path: "weather",
+    element: <Weather></Weather>}
+    ]
+  },
+]);
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />
 )
