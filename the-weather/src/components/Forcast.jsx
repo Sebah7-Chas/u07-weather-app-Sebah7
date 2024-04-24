04045c63a9187636b01f4bf5759a89d8cba5dc06
Sebah Abubeker <sebahismail21@gmail.com>
2024-04-24 14:02:40 +0200

    Forcast comp start, commit to checkout

---
+++ the-weather/src/components/Forcast.jsx
@@ -0,0 +1,19 @@
+
+const Forcast = () => {
+  return (
+    <>
+     <div className='flex items-center justify-start my-6'>
+      <p className='text-white font-meduim uppercase'>
+        Hourly Forcast</p>
+    </div>
+      <hr className='my-2' />
+
+      <div>
+
+      </div>
+
+    </>
+  )
+}
+
+export default Forcast
