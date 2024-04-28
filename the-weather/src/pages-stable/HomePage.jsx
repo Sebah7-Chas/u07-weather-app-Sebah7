671ff20ca8426de77b980f9629f1094314c66bca
Sebah Abubeker <sebahismail21@gmail.com>
2024-04-28 17:55:01 +0200

    bg-fix

--- the-weather/src/pages-stable/HomePage.jsx
+++ the-weather/src/pages-stable/HomePage.jsx
@@ -3,14 +3,15 @@ import Weather from '../components/search/WeatherSearch'
 import Results from '../components/result/SearchResults'
 import Forcast from '../components/result/ForcastResults'
 import { Outlet } from 'react-router-dom'
-import Thunder from "../assets/thunder.png";
+// import Thunder from "../assets/thunder.png";
+import Sunset from "../assets/sunset.png";
 import LineGraph from '../components/result/GraphChart';
 
 function HomePage() {
   return (
     <div className="min-h-screen bg-cover bg-no-repeat bg-center" 
     style={{backgroundImage: 
-    `url(${Thunder})`
+    `url(${Sunset})`
     }}>
 
     <div className="mx-auto max-w-screen py-2 px-4 sm:px-6 lg:px-8">
